efd622367a51677c584499e28c4c770b2b5ca7bb
Chris <frostbyte07@gmail.com>
2017-02-07 17:29:15 -0500

    Update to globals.php
    Variable being used without being set first.

--- lib/globals.php
+++ lib/globals.php
@@ -2,7 +2,7 @@
 
 $config_file = getenv('CONFIG_FILE');
 if(!$config_file) $config_file = 'config/single.conf.json';
-$GLOBALS['CONFIG'] = json_decode(file_get_contents($config_file), true);
+$CONFIG = $GLOBALS['CONFIG'] = json_decode(file_get_contents($config_file), true);
 
 $GLOBALS['BROWSERSTACK_USERNAME'] = getenv('BROWSERSTACK_USERNAME');
 if(!$GLOBALS['BROWSERSTACK_USERNAME']) $GLOBALS['BROWSERSTACK_USERNAME'] = $CONFIG['user'];
